95225797d1aed4305f86707880a79cb4699125d5
libailb <libailb@outlook.com>
2019-03-20 20:08:28 +0800

    页面增加

--- AndroidFile/app/src/main/java/test/app/libai/myapp/MainActivity.java
+++ AndroidFile/app/src/main/java/test/app/libai/myapp/MainActivity.java
@@ -19,7 +19,16 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+   //温度页面按钮
     Button btn_temp;
+    //湿度页面按钮
+    Button btn_humidity;
+    //光照页面按钮
+    Button btn_light;
+    //压力页面按钮
+    Button btn_pressure;
+
+
     Runnable runnable=new Runnable() {
         @Override
         public void run() {
@@ -53,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
             String h=mp.get("humidity");
             String l=mp.get("light");
             String p=mp.get("pressure");
-            ((TextView)findViewById(R.id.temperature)).setText("温度："+t);
-            ((TextView)findViewById(R.id.humidity)).setText("湿度："+h);
-            ((TextView)findViewById(R.id.light)).setText("光强："+l);
-            ((TextView)findViewById(R.id.pressure)).setText("压力："+p);
+            btn_temp.setText("当前温度："+t);
+            btn_humidity.setText("当前湿度："+h);
+            btn_light.setText("当前光强："+l);
+            btn_pressure.setText("当前压力："+p);
 //            String str = "查询不存在";
 //            if(message.what == 1) str = "查询成功";
 //            Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
@@ -71,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         new Thread(runnable).start();
 
-        btn_temp=findViewById(R.id.btn_start_temp);
+        btn_temp=findViewById(R.id.btn_temp);
         btn_temp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -82,6 +91,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_humidity=findViewById(R.id.btn_humidity);
+        btn_humidity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //在这里转到对应的Activity
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,HumidityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_light=findViewById(R.id.btn_light);
+        btn_light.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //在这里转到对应的Activity
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,LightActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_pressure=findViewById(R.id.btn_pressure);
+        btn_pressure.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //在这里转到对应的Activity
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,PressureActivity.class);
+                startActivity(intent);
+            }
+        });
 
                 }
 
