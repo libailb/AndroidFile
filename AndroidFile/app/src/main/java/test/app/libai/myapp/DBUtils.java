9238c57edeb3e84e435b98d09d643f584e604a8a
libailb <libailb@outlook.com>
2019-03-21 21:43:17 +0800

    多节点

--- AndroidFile/app/src/main/java/test/app/libai/myapp/DBUtils.java
+++ AndroidFile/app/src/main/java/test/app/libai/myapp/DBUtils.java
@@ -7,7 +7,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * git status
@@ -19,8 +21,8 @@ public class DBUtils {
         try {
             Class.forName("com.mysql.jdbc.Driver"); //加载驱动
             conn = DriverManager.getConnection(
-                    "jdbc:mysql://47.102.123.76:3306/libai",
-                    "root", "1234");
+                    "jdbc:mysql://47.107.254.199:3306/demo",
+                    "root", "yanyuqinghe");
         } catch (SQLException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
@@ -28,32 +30,70 @@ public class DBUtils {
         }
         return conn;
     }
-    public static HashMap<String, String> getData() {
-        HashMap<String, String> map = new HashMap<>();
+    public static  List<HashMap<String,Float>> getData() {
         Connection conn = getConnection();
+        List<HashMap<String,Float>> list =new ArrayList<>();
         try {
             Statement st = conn.createStatement();
-            String sql = "select * from data";
-            ResultSet res = st.executeQuery(sql);
-            if (res == null) {
+            String sql1 = "SELECT * FROM aliyun1 WHERE id = (SELECT MAX(id) FROM aliyun1);";
+            String sql2 = "SELECT * FROM aliyun2 WHERE id = (SELECT MAX(id) FROM aliyun2);";
+            String sql3 = "SELECT * FROM aliyun3 WHERE id = (SELECT MAX(id) FROM aliyun3);";
+            String sql4 = "SELECT * FROM aliyun4 WHERE id = (SELECT MAX(id) FROM aliyun4);";
+
+            ResultSet res1 = st.executeQuery(sql1);
+            ResultSet res2 = st.executeQuery(sql2);
+            ResultSet res3 = st.executeQuery(sql3);
+            ResultSet res4 = st.executeQuery(sql4);
+
+            if (res1 == null) {
                 return null;
             } else {
-                int cnt = res.getMetaData().getColumnCount();
-                //res.last(); int rowCnt = res.getRow(); res.first();
-                res.next();
-                for (int i = 1; i <= cnt; ++i) {
-                    String field = res.getMetaData().getColumnName(i);
-                    map.put(field, res.getString(field));
+                    HashMap<String,Float> map = new HashMap<>();
+                    res1.next();
+                    map.put("temp",res1.getFloat("temp"));
+                    map.put("humi",res1.getFloat("humi"));
+                    list.add(map);
                 }
+            if (res2 == null) {
+                return null;
+            } else {
+                HashMap<String,Float> map = new HashMap<>();
+                res1.next();
+                map.put("temp",res2.getFloat("temp"));
+                map.put("humi",res2.getFloat("humi"));
+                list.add(map);
+            }
+            if (res3 == null) {
+                return null;
+            } else {
+                HashMap<String,Float> map = new HashMap<>();
+                res1.next();
+                map.put("temp",res3.getFloat("temp"));
+                map.put("humi",res3.getFloat("humi"));
+                list.add(map);
+            }
+            if (res4 == null) {
+                return null;
+            } else {
+                HashMap<String,Float> map = new HashMap<>();
+                res1.next();
+                map.put("temp",res4.getFloat("temp"));
+                map.put("humi",res4.getFloat("humi"));
+                list.add(map);
+            }
                 conn.close();
                 st.close();
-                res.close();
-                return map;
-            }
-        } catch (Exception e) {
+                res1.close();
+            res2.close();res3.close();res4.close();
+
+                return list;
+            } catch (Exception e) {
             e.printStackTrace();
             Log.d(TAG, " 数据操作异常");
             return null;
         }
     }
+
+
+
 }
--- AndroidFile/app/src/main/java/test/app/libai/myapp/MainActivity.java
+++ AndroidFile/app/src/main/java/test/app/libai/myapp/MainActivity.java
@@ -12,7 +12,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,33 +22,36 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
    //温度页面按钮
-    Button btn_temp;
+    Button btn_temp1;
     //湿度页面按钮
-    Button btn_humidity;
-    //光照页面按钮
-    Button btn_light;
-    //压力页面按钮
-    Button btn_pressure;
+    Button btn_humidity1;
 
+    Button btn_temp2;
+    Button btn_humidity2;
+
+    Button btn_temp3;
+    Button btn_humidity3;
+
+    Button btn_temp4;
+    Button btn_humidity4;
 
     Runnable runnable=new Runnable() {
         @Override
         public void run() {
             while(true) {
                 try {
-                    HashMap<String, String> mp =
-                            DBUtils.getData();
+                    List< HashMap<String, Float>> list =DBUtils.getData();
                     Message msg = new Message();
-                    if (mp == null) {
+                    if (list == null) {
                         msg.what = 0;
                         msg.obj = null;
                         //非UI线程不要试着去操作界面
                     } else {
                         msg.what = 1;
-                        msg.obj = mp;
+                        msg.obj = list;
                     }
                     handler.sendMessage(msg);
-                    Thread.sleep(500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -57,22 +62,51 @@ public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message message) {
-            HashMap<String, String> mp =(HashMap<String,String>)message.obj;
-            String t=mp.get("temperature");
-            String h=mp.get("humidity");
-            String l=mp.get("light");
-            String p=mp.get("pressure");
-            btn_temp.setText("当前温度："+t);
-            btn_humidity.setText("当前湿度："+h);
-            btn_light.setText("当前光强："+l);
-            btn_pressure.setText("当前压力："+p);
-//            String str = "查询不存在";
-//            if(message.what == 1) str = "查询成功";
-//            Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            List<HashMap<String,Float>> list=(List<HashMap<String,Float>>)message.obj;
+            HashMap<String, Float> mp1 =list.get(0);
+                if(mp1 == null){
+                    btn_temp1.setText("温度获取失败");
+                    btn_humidity1.setText("湿度获取失败");
+                } else{
+               Float t=mp1.get("temp");
+               Float h=mp1.get("humi");
+                btn_temp1.setText(t+"℃");
+                btn_humidity1.setText(h+"%");
+            }
+            HashMap<String, Float> mp2 =list.get(1);
+            if(mp2 == null){
+                btn_temp2.setText("温度获取失败");
+                btn_humidity2.setText("湿度获取失败");
+            } else{
+                Float t=mp2.get("temp");
+                Float h=mp2.get("humi");
+                btn_temp2.setText(t+"℃");
+                btn_humidity2.setText(h+"%");
+            }
+            HashMap<String, Float> mp3 =list.get(2);
+            if(mp3 == null){
+                btn_temp3.setText("温度获取失败");
+                btn_humidity3.setText("湿度获取失败");
+            } else{
+                Float t=mp3.get("temp");
+                Float h=mp3.get("humi");
+                btn_temp3.setText(t+"℃");
+                btn_humidity3.setText(h+"%");
+            }
+            HashMap<String,  Float> mp4 =list.get(3);
+            if(mp4 == null){
+                btn_temp4.setText("温度获取失败");
+                btn_humidity4.setText("湿度获取失败");
+            } else{
+                Float t=mp4.get("temp");
+                Float h=mp4.get("humi");
+                btn_temp4.setText(t+"℃");
+                btn_humidity4.setText(h+"%");
+            }
+
             return false;
         }
     });
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,8 +114,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         new Thread(runnable).start();
 
-        btn_temp=findViewById(R.id.btn_temp);
-        btn_temp.setOnClickListener(new View.OnClickListener(){
+        btn_temp1=findViewById(R.id.btn_temp1);
+        btn_temp1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 //在这里转到对应的Activity
@@ -91,8 +125,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_humidity=findViewById(R.id.btn_humidity);
-        btn_humidity.setOnClickListener(new View.OnClickListener(){
+        btn_humidity1=findViewById(R.id.btn_humidity1);
+        btn_humidity1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 //在这里转到对应的Activity
@@ -102,27 +136,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_light=findViewById(R.id.btn_light);
-        btn_light.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //在这里转到对应的Activity
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,LightActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_pressure=findViewById(R.id.btn_pressure);
-        btn_pressure.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //在这里转到对应的Activity
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,PressureActivity.class);
-                startActivity(intent);
-            }
-        });
 
                 }
 
